bf7a7320930f8735790a58e03164d375f9c02a20
lebansty <lebanstyv@gmail.com>
2022-12-23 10:06:52 +0530

    vercel change

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { io } from 'socket.io-client';
 import './App.css';
 import Chat from './Chat';
 
-const socket =io.connect("http://localhost:3001")
+const socket =io.connect("https://chatapp-backend-chi.vercel.app/")
 
 function App() {
 const [userName,setUserName] =useState("");
